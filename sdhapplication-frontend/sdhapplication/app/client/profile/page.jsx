3fcb51a096e6b4de07a96d46839feb0d6fce5618
sandipan898 <sandipan.das898@gmail.com>
2024-02-13 02:26:12 +0530

    Updating client app

--- sdhapplication-frontend/sdhapplication/app/client/profile/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ClientProfilePage = () => {
-  return (
-    <div>ClientProfilePage</div>
-  )
-}
-
-export default ClientProfilePage
--- sdhapplication-frontend/sdhapplication/app/client/search/page.jsx
+++ sdhapplication-frontend/sdhapplication/app/client/search/page.jsx
@@ -2,10 +2,13 @@
 
 import ClientSearch from "@components/client/ClientSearch";
 import ClientTable from "@components/client/ClientTable";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getClients } from "@services/ClientServices";
 
 const ClientSearchPage = () => {
     const [searchClicked, setsearchClicked] = useState(false);
+    const [clients, setClients] = useState([]);
+
     const handleSearchSubmit = (e) => {
       e.preventDefault();
       setsearchClicked(true);
@@ -15,11 +18,21 @@ const ClientSearchPage = () => {
       setsearchClicked(false);
     }
 
+    const fetchClients = async () => {
+      const clients = await getClients();
+      console.log("fetchClients", clients);
+      setClients(clients);
+    }
+
+    useEffect(() => {
+      fetchClients();
+    }, [])
+
   return (
     <div className="mx-10">
         <ClientSearch handleSearchSubmit={handleSearchSubmit} searchClicked={searchClicked} handleClearSearch={handleClearSearch} />
         {
-            searchClicked && <ClientTable />
+            searchClicked && <ClientTable clients={clients} />
         }
     </div>
   )
