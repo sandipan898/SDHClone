993df00e85d373a069c78f76cf2ae1048878b138
sandipan898 <sandipan.das898@gmail.com>
2024-02-16 20:47:39 +0530

    updating offer module

--- sdhapplication-beckend/sdhapplication/src/main/java/com/ste/sdhapplication/offermodule/controller/OOSController.java
+++ sdhapplication-beckend/sdhapplication/src/main/java/com/ste/sdhapplication/offermodule/controller/OOSController.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/v1/oos")
 public class OOSController {
     @Autowired
--- sdhapplication-beckend/sdhapplication/src/main/java/com/ste/sdhapplication/offermodule/controller/OfferController.java
+++ sdhapplication-beckend/sdhapplication/src/main/java/com/ste/sdhapplication/offermodule/controller/OfferController.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/v1/offer")
 public class OfferController {
     @Autowired
