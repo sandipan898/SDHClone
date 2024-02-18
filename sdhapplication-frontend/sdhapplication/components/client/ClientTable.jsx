ae2c4066b1b12e6a0cc8d2a8c74d5a024f3726a9
Das <sandipan.b.das@capgemini.com>
2024-02-18 16:24:14 +0530

    update client component

--- sdhapplication-frontend/sdhapplication/components/client/ClientTable.jsx
+++ sdhapplication-frontend/sdhapplication/components/client/ClientTable.jsx
@@ -91,7 +91,7 @@ const ClientTable = ({ clients }) => {
                             </td>
                             <td className="px-6 py-4 dark:text-white">
                                 <button className="inline-flex items-center justify-center mx-5 px-5 py-3 text-base font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-blue-900">               
-                                    <svg className="h-8 w-8 text-red-500"  viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />  <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />  <line x1="16" y1="5" x2="19" y2="8" /></svg>
+                                    <svg className="h-5 w-5 text-white"  viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />  <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />  <line x1="16" y1="5" x2="19" y2="8" /></svg>
                                 </button>
                             </td>
                         </tr>
