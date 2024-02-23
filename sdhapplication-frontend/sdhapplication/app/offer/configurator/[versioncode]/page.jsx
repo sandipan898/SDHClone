62ff3f43e17502c11c12491e823e802b0760312f
Das <sandipan.b.das@capgemini.com>
2024-02-23 08:11:08 +0530

    updating configurator app

--- sdhapplication-frontend/sdhapplication/app/offer/configurator/[versioncode]/page.jsx
+++ sdhapplication-frontend/sdhapplication/app/offer/configurator/[versioncode]/page.jsx
@@ -1,12 +1,33 @@
 "use client";
 
-import React from 'react'
+import Configurator from '@components/offer/Configurator';
+import { getVersionByCode } from '@services/VehicleServices';
+import React, { useEffect, useState } from 'react'
 
 const ConfiguratorPage = ({ params }) => {
     const versioncode = params.versioncode;
 
+    const [version, setVersion] = useState({})
+
+    const fetchVersion = async () => {
+        getVersionByCode(versioncode)
+            .then(data => {
+                console.log("fetchVersion", data);
+                setVersion(data);
+            })
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {
+        fetchVersion();
+    }, [])
+
     return (
-        <div>ConfiguratorPage</div>
+        <div className='md:m-5 sm:my-5'>
+            <div className="border border-gray-200 rounded-lg shadow">
+                <Configurator version={version} />
+            </div>
+        </div>
     )
 }
 
