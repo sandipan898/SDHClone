5509870463c7cd086b1967b15df47a028997b0b3
Das <sandipan.b.das@capgemini.com>
2024-02-23 08:11:25 +0530

    updating family app

--- sdhapplication-frontend/sdhapplication/app/offer/family/[type]/[code]/page.jsx
+++ sdhapplication-frontend/sdhapplication/app/offer/family/[type]/[code]/page.jsx
@@ -3,13 +3,15 @@
 import VersionList from '@components/vehicle/VersionList';
 import VersionSearch from '@components/vehicle/VersionSearch';
 import { getVersionByFamilyCode } from '@services/VehicleServices';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
 const VersionPage = ({ params }) => {
   const familyCode = params?.code;
   const [versions, setVersions] = useState([])
   const [filteredVersions, setFilteredVersions] = useState(versions)
-  const [versionSearchTerm, setVersionSearchTerm] = useState(versions)
+  const [versionSearchTerm, setVersionSearchTerm] = useState("")
+  const [searchTimeout, setSearchTimeout] = useState(null);
 
   const fetchVersions = async () => {
     getVersionByFamilyCode(familyCode)
@@ -25,20 +27,24 @@ const VersionPage = ({ params }) => {
     fetchVersions();
   }, [])
 
-  useEffect(() => {
-    handleVersionSearchChange()
-  }, [versionSearchTerm])
+  const filterVersions = (searchTerm) => {
+    const regex = new RegExp(searchTerm, "i");
+    return versions.filter(
+      (versions) =>
+        regex.test(versions.code) ||
+        regex.test(versions.versionName)
+    );
+  }
   
+  const handleVersionSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setVersionSearchTerm(e.target.value);
 
-  const handleVersionSearchChange = () => {
-    if(versionSearchTerm === "") {
-      setFilteredVersions(versions)
-    } else {
-      let regex = new RegExp(/^[a-zA-Z]+$/);
-
-      const filteredVersions = versions.filter(version => regex.test(versionSearchTerm))
+    setSearchTimeout(
+    setTimeout(() => {
+      const filteredVersions = filterVersions(e.target.value);
       setFilteredVersions(filteredVersions);
-    }
+    }, 500))
   }
 
   const family = versions && versions.length && versions[0].family;
@@ -48,8 +54,15 @@ const VersionPage = ({ params }) => {
       <h1 className="my-10 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-white">{family.familyName} - {params?.code?.toUpperCase()} Versions</h1>
       <div className="md:w-4/5 mx-auto">
         <div class="relative overflow-x-auto border border-gray-200 rounded-lg shadow sm:rounded-lg p-4">
-          <VersionSearch setVersionSearchTerm={setVersionSearchTerm} />
-          <VersionList params={params} versions={filteredVersions} handleVersionSearchChange={handleVersionSearchChange} family={family} />
+          
+          <Link href={`/offer/family/${params?.type}`} class="inline-flex w-100 items-center mt-4 px-3 py-2 text-sm text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
+          </svg>
+            {params?.type.toUpperCase()} Family 
+          </Link>
+          <VersionSearch handleVersionSearchChange={handleVersionSearchChange} versionSearchTerm={versionSearchTerm} />
+          <VersionList params={params} versions={filteredVersions} family={family} />
         </div>
       </div>
     </div>
--- sdhapplication-frontend/sdhapplication/app/offer/family/[type]/page.jsx
+++ sdhapplication-frontend/sdhapplication/app/offer/family/[type]/page.jsx
@@ -2,14 +2,16 @@
 
 import FamilyList from '@components/vehicle/FamilyList';
 import { getFamilyByDealer } from '@services/VehicleServices';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
 const FamilyPage = ({ params }) => {
 
     const [families, setFamilies] = useState([])
     const [filteredFamilies, setFilteredFamilies] = useState(families)
-    const [familySearchTerm, setFamilySearchTerm] = useState(families)
-    
+    const [familySearchTerm, setFamilySearchTerm] = useState("")
+    const [searchTimeout, setSearchTimeout] = useState(null);
+
     const fetchFamilies = async () => {
         getFamilyByDealer("999999")
             .then(data => {
@@ -24,27 +26,35 @@ const FamilyPage = ({ params }) => {
         fetchFamilies();
     }, [])
 
-    useEffect(() => {
-        handleFamilySearchChange();
-    }, [familySearchTerm])
-    
-
-    const handleFamilySearchChange = () => {        
-        if(familySearchTerm === "") {
-            setFilteredFamilies(families)
-        } else {
-            let regex = new RegExp(/^[a-zA-Z]+$/);
- 
-            const filteredFamilies = families.filter(family => regex.test(familySearchTerm))
-            setFilteredFamilies(filteredFamilies);
-        }
+    const filterFamily = (searchTerm) => {
+        const regex = new RegExp(searchTerm, "i");
+        return families.filter(
+            (family) =>
+                regex.test(family.code) ||
+                regex.test(family.familyName) ||
+                regex.test(family.familyType)
+        );
     }
+
+    const handleFamilySearchChange = (e) => {
+        clearTimeout(searchTimeout);
+        setFamilySearchTerm(e.target.value);
+
+        setSearchTimeout(
+            setTimeout(() => {
+                const filteredFamily = filterFamily(e.target.value);
+                setFilteredFamilies(filteredFamily);
+            }, 500)
+        )
+    }
+
+    const clientId = localStorage?.getItem('clientId');
 
     return (
         <div className="md:m-5 sm:my-5">
             <h1 className="my-10 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-white">{params?.type.toUpperCase()} Family</h1>
             <div className="md:w-4/5 mx-auto">
-                <FamilyList params={params} families={filteredFamilies} setFamilySearchTerm={setFamilySearchTerm} />
+                <FamilyList clientId={clientId} familySearchTerm={familySearchTerm} params={params} families={filteredFamilies} handleFamilySearchChange={handleFamilySearchChange} />
             </div>
         </div>
     )
