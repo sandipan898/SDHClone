fb2dd04eac7bb1e9a2ca4d717bb7b3c891ca517a
sandipan898 <sandipan.das898@gmail.com>
2024-02-25 20:17:58 +0530

    Adding discount, personalization, transformation components

---
+++ sdhapplication-frontend/sdhapplication/components/offer/personalization/Personalization.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from 'react'
+
+const Personalization = () => {
+    return (
+        <div>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+                Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons,
+                dropdowns, modals, navbars, and more.
+            </p>
+            <p className="text-gray-500 dark:text-gray-400">
+                Check out this guide to learn how to&nbsp;
+                <a
+                    href="https://flowbite.com/docs/getting-started/introduction/"
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                    get started&nbsp;
+                </a>
+                and start developing websites even faster with components on top of Tailwind CSS.
+            </p>
+        </div>
+    )
+}
+
+export default Personalization
