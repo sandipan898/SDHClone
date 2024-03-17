b857761d3f717e1ea09d0fd9e0668658bc2a8d49
sandipan898 <sandipan.das898@gmail.com>
2024-03-18 01:07:23 +0530

    updating offer module

--- sdhapplication-beckend/sdhapplication/src/main/java/com/ste/sdhapplication/offermodule/model/OfferModel.java
+++ sdhapplication-beckend/sdhapplication/src/main/java/com/ste/sdhapplication/offermodule/model/OfferModel.java
@@ -32,6 +32,6 @@ public class OfferModel {
     private String option;
     private String colorPrice;
     private String trimPrice;
-    private String totalOptionPrice;
+    private String optionPrice;
     private double totalPrice;
 }
